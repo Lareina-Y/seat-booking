2841d6cf4a15fb9946eeb35c4c475b734b4bd4ab
”Shiwen <fairy3219889700@gmail.com>
2022-12-24 17:06:27 -0400

    add logo

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,6 @@
 	 * @desc:	This file is the header of the website
 	 */
 
-
-
   // Include folder Access control 
 	// citation from CSCI2170 A4 solution by Dr. Raghav V. Sampangi, =========================
 	$currentDirectory = getcwd();
@@ -36,8 +34,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>DCSSA</title>
-	<link rel="icon" href="img/Fevicon.png" type="image/png">
+  <title>DCSSA | 2023春晚订票</title>
+	<link rel="icon" href="img/logo.png" type="image/png">
   
   <!-- add for the project purpose -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
